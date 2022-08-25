eb3e88446896ff5757a2602f2ce4b714e2f109e7
Bradley Showen <bshowen@live.seminolestate.edu>
2022-08-25 15:14:10 -0400

    Add nav with basic navigation

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
+import { NavBar } from "./components/NavBar";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <NavBar />
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="products" element={<p>Products</p>} />
+        <Route path="cart" element={<p>Cart</p>} />
         <Route
           path="*"
           element={
