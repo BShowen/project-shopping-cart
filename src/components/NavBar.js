9d71f1e782f6340aa84297b049ce6cf1edc899cc
Bradley Showen <BShowen@me.com>
2022-08-28 09:23:47 -0400

    Make nav bar sticky

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,89 +2,70 @@ import { NavLink } from "react-router-dom";
 
 export function NavBar({ toggleCart }) {
   return (
-    <nav className="navbar navbar-expand-sm bg-light">
-      <div className="container-fluid">
-        <NavLink className="navbar-brand" to="/">
-          NavBar
+    <nav className="navbar sticky-top bg-light">
+      <div className="container">
+        <NavLink className="navbar-brand me-auto" to="/">
+          Fake Store
         </NavLink>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/">
-                Home
-              </NavLink>
-            </li>
-            <li className="nav-item dropdown">
+        <NavLink className="mx-1 p-2 nav-link" to="/">
+          Home
+        </NavLink>
+        <div className="mx-1 p-2 nav-link dropdown">
+          <NavLink
+            className="nav-link dropdown-toggle"
+            data-bs-toggle="dropdown"
+            to="#"
+            role="button"
+            aria-expanded="false"
+          >
+            Products
+          </NavLink>
+          <div className="dropdown-menu">
+            <div className="dropdown-item p-0">
               <NavLink
-                className="nav-link dropdown-toggle"
-                data-bs-toggle="dropdown"
-                to="#"
-                role="button"
-                aria-expanded="false"
+                className="text-decoration-none d-block px-3 py-1"
+                to="/products"
               >
-                Products
+                All
               </NavLink>
-              <div className="dropdown-menu">
-                <div className="dropdown-item p-0">
-                  <NavLink
-                    className="text-decoration-none d-block px-3 py-1"
-                    to="/products"
-                  >
-                    All
-                  </NavLink>
-                </div>
-                <div className="dropdown-item p-0">
-                  <NavLink
-                    className="text-decoration-none d-block px-3 py-1"
-                    to="/products?category=mens"
-                  >
-                    Mens
-                  </NavLink>
-                </div>
-                <div className="dropdown-item p-0">
-                  <NavLink
-                    className="text-decoration-none d-block px-3 py-1"
-                    to="/products?category=womens"
-                  >
-                    Womens
-                  </NavLink>
-                </div>
-                <div className="dropdown-item p-0">
-                  <NavLink
-                    className="text-decoration-none d-block px-3 py-1"
-                    to="/products?category=electronics"
-                  >
-                    Electronics
-                  </NavLink>
-                </div>
-                <div className="dropdown-item p-0">
-                  <NavLink
-                    className="text-decoration-none d-block px-3 py-1"
-                    to="/products?category=jewelry"
-                  >
-                    Jewelry
-                  </NavLink>
-                </div>
-              </div>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="#" onClick={toggleCart}>
-                Cart
+            </div>
+            <div className="dropdown-item p-0">
+              <NavLink
+                className="text-decoration-none d-block px-3 py-1"
+                to="/products?category=mens"
+              >
+                Mens
               </NavLink>
-            </li>
-          </ul>
+            </div>
+            <div className="dropdown-item p-0">
+              <NavLink
+                className="text-decoration-none d-block px-3 py-1"
+                to="/products?category=womens"
+              >
+                Womens
+              </NavLink>
+            </div>
+            <div className="dropdown-item p-0">
+              <NavLink
+                className="text-decoration-none d-block px-3 py-1"
+                to="/products?category=electronics"
+              >
+                Electronics
+              </NavLink>
+            </div>
+            <div className="dropdown-item p-0">
+              <NavLink
+                className="text-decoration-none d-block px-3 py-1"
+                to="/products?category=jewelry"
+              >
+                Jewelry
+              </NavLink>
+            </div>
+          </div>
         </div>
+        <NavLink className="mx-1 p-2 nav-link" to="#" onClick={toggleCart}>
+          Cart
+        </NavLink>
       </div>
     </nav>
   );
