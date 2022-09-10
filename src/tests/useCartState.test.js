91e85caa90009c97644817709f1ee2567381dc21
Bradley Showen <BShowen@me.com>
2022-09-10 14:32:43 -0400

    remove unnecessary test for errors

--- src/tests/useCartState.test.js
+++ src/tests/useCartState.test.js
@@ -12,23 +12,15 @@ describe("The cart component", () => {
     const { result } = renderHook(() => useCartState());
 
     await act(async () => {
-      await result.current.dispatchToCart({ id: 2, type: "increment" });
+      result.current.dispatchToCart({ id: 2, type: "increment" });
     });
 
     await waitFor(() => expect(result.current.products.length).toBe(1));
 
     await act(async () => {
-      await result.current.dispatchToCart({ id: 1, type: "increment" });
+      result.current.dispatchToCart({ id: 1, type: "increment" });
     });
 
     await waitFor(() => expect(result.current.products.length).toBe(2));
-  });
-
-  it("should have an error", async () => {
-    const { result } = renderHook(() => useCartState());
-
-    await act(async () => {
-      result.current.dispatchToCart({ id: 0, type: "increment" });
-    });
   });
 });
